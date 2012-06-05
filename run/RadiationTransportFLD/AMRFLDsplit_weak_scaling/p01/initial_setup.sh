9f3814d588ddeddc6f603bfe00317d505f9b2335
Daniel R. Reynolds <reynolds@smu.edu>
2012-06-05 13:10:04 -0500

    Added directory for weak scaling tests of AMRFLDSplit solver

---
+++ run/RadiationTransportFLD/AMRFLDsplit_weak_scaling/p01/initial_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NUMPROCS=1
+
+
+sed '/StopTime/ d' RHIonization2_scaling.enzo > tmp.enzo
+echo "StopTime = 2.0e-8" >> tmp.enzo
+
+mpiexec_intel -n $NUMPROCS ./enzo.exe -d tmp.enzo &> output_init.txt
+
+sed 's:StaticHierarchy     = 0:StaticHierarchy     = 1:' DD0001/data0001 > tmp
+grep StopTime RHIonization2_scaling.enzo > stoptime.txt
+sed '/StopTime/ d' tmp > tmp.enzo
+cat stoptime.txt >> tmp.enzo
+\rm stoptime.txt
+mv tmp.enzo DD0001/data0001
