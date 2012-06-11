8b5ad6820ce7d27377dad90345a0da0ecdefd861
drreynolds <drreynolds@localhost>
2012-06-11 15:50:00 -0500

    Updated FLD weak scaling test scripts

--- run/RadiationTransportFLD/AMRFLDsplit_weak_scaling/p16/initial_setup.sh
+++ run/RadiationTransportFLD/AMRFLDsplit_weak_scaling/p16/initial_setup.sh
@@ -11,6 +11,7 @@ mpiexec_intel -n $NUMPROCS ./enzo.exe -d tmp.enzo &> output_init.txt
 sed 's:StaticHierarchy     = 0:StaticHierarchy     = 1:' DD0001/data0001 > tmp
 grep StopTime RHIonization2_scaling.enzo > stoptime.txt
 sed '/StopTime/ d' tmp > tmp.enzo
+\rm tmp
 cat stoptime.txt >> tmp.enzo
 \rm stoptime.txt
 mv tmp.enzo DD0001/data0001
