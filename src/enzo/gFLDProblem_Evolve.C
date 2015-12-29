0bff4c2a1a43e2409afb2d430dd1635e99f45111
drreynolds <drreynolds@localhost>
2015-12-29 11:13:20 -0600

    Minor bug-fixes in gFLDSplit and gFLDProblem solvers

--- src/enzo/gFLDProblem_Evolve.C
+++ src/enzo/gFLDProblem_Evolve.C
@@ -198,7 +198,7 @@ int gFLDProblem::Evolve(LevelHierarchyEntry *LevelArray[], int level,
       // update HeI, HeII as appropriate fractions of 'true' density
       nHeI[i]   *= rho[i]*(1.0-HFrac)/rhochem;
       nHeII[i]  *= rho[i]*(1.0-HFrac)/rhochem;
-      nHeIII[i] *= max(0.0, rho[i]*(1.0-HFrac) - nHeI[i] - nHeII[i]);
+      nHeIII[i] = max(0.0, rho[i]*(1.0-HFrac) - nHeI[i] - nHeII[i]);
     }
   }
 
